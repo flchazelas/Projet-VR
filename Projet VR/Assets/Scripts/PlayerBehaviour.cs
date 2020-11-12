2ca188e895b80737a892e17288beabfa2c24e83b
Floran Chazelas <flo.chazelas@orange.fr>
2020-11-12 10:14:43 +0100

    Ajout scene

---
+++ Projet VR/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    private Camera mainCamera;
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + mainCamera.transform.forward.x * speed * Time.deltaTime, 1, transform.position.z + mainCamera.transform.forward.z * speed * Time.deltaTime);
+    }
+}
